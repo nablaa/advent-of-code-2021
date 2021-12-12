4026f426ae123c35c5074873b23bcd63e657dfe8
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2021-12-13 00:25:29 +0100

    12 part 2

--- 12.pl
+++ 12.pl
@@ -1,6 +1,5 @@
 % Input:
 %%%
-
 edge(start,a).
 edge(start,b).
 edge(a,c).
@@ -22,23 +21,36 @@ adjacent(X, Y) :- edge(X, Y).
 adjacent(X, Y) :- edge(Y, X).
 
 path(Start, End, Path) :-
-	traverse(Start, End, [Start], Path).
+	traverse(Start, End, [Start], [], Reversed),
+	reverse(Reversed, Path).
 
-traverse(X, Y, Visited, [Y|Visited]) :-
+traverse(X, Y, Visited, _, [Y|Visited]) :-
 	adjacent(X, Y).
 
-traverse(X, Y, Visited, Path) :-
+traverse(X, Y, Visited, Single, Path) :-
 	adjacent(X, Z),
 	Z \== Y,
 	small(Z),
 	not(member(Z, Visited)),
-	traverse(Z, Y, [Z|Visited], Path).
+	traverse(Z, Y, [Z|Visited], Single, Path).
 
-traverse(X, Y, Visited, Path) :-
+traverse(X, Y, Visited, Single, Path) :-
+	adjacent(X, Z),
+	Z \== Y,
+	Z \== start,
+	Z \== end,
+	small(Z),
+	member(Z, Visited),
+	length(Single, L),
+	L < 1,
+	not(member(Z, Single)),
+	traverse(Z, Y, [Z|Visited], [Z|Single], Path).
+
+traverse(X, Y, Visited, Single, Path) :-
 	adjacent(X, Z),
 	Z \== Y,
 	big(Z),
-	traverse(Z, Y, [Z|Visited], Path).
+	traverse(Z, Y, [Z|Visited], Single, Path).
 
 pathcount(Start, End, Count) :-
 	aggregate_all(count, path(Start, End, _), Count).

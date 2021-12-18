43d9f5488011b2cb074c9e0416038937d8010791
Miika-Petteri Matikainen <miikapetteri@gmail.com>
2021-12-18 18:20:51 +0100

    18 wip

--- 18.hs
+++ 18.hs
@@ -1,34 +1,21 @@
 import Text.Parsec
 import Text.Parsec.String
+import Data.Maybe
 
-data Snailfish = Pair Element Element
+data Element = Pair Element Element | Number Int
         deriving (Eq)
-data Element = Elements Snailfish | Number Int
-        deriving (Eq)
-
-instance Show Snailfish where
-        show (Pair e1 e2) = "[" ++ show e1 ++ "," ++ show e2 ++ "]"
 
 instance Show Element where
-        show (Elements s) = show s
+        show (Pair e1 e2) = "[" ++ show e1 ++ "," ++ show e2 ++ "]"
         show (Number n) = show n
 
-parseInput :: String -> [Snailfish]
+parseInput :: String -> [Element]
 parseInput = map parseSnailfish . lines
 
-parseSnailfish :: String -> Snailfish
-parseSnailfish str = case parse snailfish "" str of
+parseSnailfish :: String -> Element
+parseSnailfish str = case parse element "" str of
                        Right s -> s
                        Left err -> error $ show err
-
-snailfish :: Parser Snailfish
-snailfish = do
-        _ <- char '['
-        e1 <- element
-        _ <- char ','
-        e2 <- element
-        _ <- char ']'
-        return $ Pair e1 e2
 
 element :: Parser Element
 element = elements <|> number
@@ -39,8 +26,90 @@ number = do
         return $ Number (read n)
 
 elements :: Parser Element
-elements = do Elements <$> snailfish
+elements = do
+        _ <- char '['
+        e1 <- element
+        _ <- char ','
+        e2 <- element
+        _ <- char ']'
+        return $ Pair e1 e2
 
+addition :: Element -> Element -> Element
+addition = Pair
+
+reduce :: Element -> Element
+reduce = undefined
+
+hasPairNestedNDeep :: Int -> Element -> Bool
+hasPairNestedNDeep 0 (Pair (Number _) (Number _)) = True
+hasPairNestedNDeep 0 _ = False
+hasPairNestedNDeep n (Pair e1 e2) = hasPairNestedNDeep (n - 1) e1 || hasPairNestedNDeep (n - 1) e2
+hasPairNestedNDeep n (Number _) = False
+
+data Trail = LeftPair Element | RightPair Element
+        deriving (Show, Eq)
+type Context = [Trail]
+type Zipper = (Element, Context)
+
+goLeft :: Zipper -> Maybe Zipper
+goLeft (Number _, _) = Nothing
+goLeft (Pair l r, trail) = Just (l, LeftPair r:trail)
+
+goRight :: Zipper -> Maybe Zipper
+goRight (Number _, _) = Nothing
+goRight (Pair l r, trail) = Just (r, RightPair l:trail)
+
+goUp :: Zipper -> Maybe Zipper
+goUp (_, []) = Nothing
+goUp (element, LeftPair r:trail) = Just (Pair element r, trail)
+goUp (element, RightPair l:trail) = Just (Pair l element, trail)
+
+topMost :: Zipper -> Zipper
+topMost zipper = maybe zipper topMost (goUp zipper)
+
+walk :: Zipper -> Context -> Zipper
+walk zipper [] = zipper
+walk zipper ((LeftPair _):cs) = walk (fromJust $ goLeft zipper) cs
+walk zipper ((RightPair _):cs) = walk (fromJust $ goRight zipper) cs
+
+explodeLeftmostPair :: Zipper -> Zipper
+explodeLeftmostPair zipper@(Number _, trail) = zipper
+explodeLeftmostPair z@(Pair (Number n1) (Number n2), trail) | length trail >= 4 = exploded
+        where newZipper = (Number 0, trail)
+              leftAdded = addLeftNumber n1 newZipper
+              t = topMost leftAdded
+              rightAdded = addRightNumber n2 (walk t (reverse trail))
+              exploded = rightAdded
+explodeLeftmostPair zipper@(_, trail) = exploded
+        where leftExploded = maybe zipper explodeLeftmostPair (goLeft zipper)
+              t = topMost leftExploded
+              t2 = topMost zipper
+              rightExploded = maybe leftExploded explodeLeftmostPair (goRight (walk t (reverse trail)))
+              exploded = if t /= t2 then leftExploded else rightExploded
+
+addLeftNumber :: Int -> Zipper -> Zipper
+addLeftNumber = traverseLeftPoint
+
+traverseLeftPoint :: Int -> Zipper -> Zipper
+traverseLeftPoint n zipper@(_, []) = zipper
+traverseLeftPoint n zipper@(_, (LeftPair _):trail) = traverseLeftPoint n (fromJust $ goUp zipper)
+traverseLeftPoint n zipper@(_, (RightPair _):trail) = addRightmostNumber n (fromJust (goUp zipper >>= goLeft))
+
+addRightmostNumber :: Int -> Zipper -> Zipper
+addRightmostNumber n (Number x, trail) = (Number (x + n), trail)
+addRightmostNumber n zipper = addRightmostNumber n (fromJust $ goRight zipper)
+
+addRightNumber :: Int -> Zipper -> Zipper
+addRightNumber = traverseRightPoint
+
+traverseRightPoint :: Int -> Zipper -> Zipper
+traverseRightPoint n zipper@(_, []) = zipper
+traverseRightPoint n zipper@(_, (RightPair _):trail) = traverseRightPoint n (fromJust $ goUp zipper)
+traverseRightPoint n zipper@(_, (LeftPair _):trail) = addLeftmostNumber n (fromJust (goUp zipper >>= goRight))
+
+addLeftmostNumber :: Int -> Zipper -> Zipper
+addLeftmostNumber n (Number x, trail) = (Number (x + n), trail)
+addLeftmostNumber n zipper = addLeftmostNumber n (fromJust $ goLeft zipper)
 
 main = do input <- getContents
           let snailfishes = parseInput input
